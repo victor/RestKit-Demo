8ce55e4572a4ce9a792784ea797f499ea3198876
Victor Jalencas <victor@carotena.net>
2011-10-27 16:15:14 +0200

    correct resource path

--- RestKitDemo/NCEMasterViewController.m
+++ RestKitDemo/NCEMasterViewController.m
@@ -34,7 +34,7 @@
 
 - (void)fetchEvents;
 {
-    [[RKObjectManager sharedManager] loadObjectsAtResourcePath:@"/events" delegate:self];
+    [[RKObjectManager sharedManager] loadObjectsAtResourcePath:@"/events.json" delegate:self];
 }
 
 
